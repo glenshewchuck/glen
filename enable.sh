04284e70bc1c213fa14d3d62f74d91bf905c73f1
Glen Shewchuck <glenshewchuck@gmail.com>
2020-08-16 12:41:40 -0700

    first commit

---
+++ enable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+drush -y en \
+exif \
+devel \
+admin_toolbar \
+backup_migrate \
+better_exposed_filters \
+diff \
+views_bulk_operations \
+pathauto \
+paragraphs \
+module_filter \
+ctools \
+entity_usage
+
---
+++ require.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+composer -v require \
+drupal/exif \
+drupal/devel \
+drupal/admin_toolbar \
+drupal/backup_migrate \
+drupal/better_exposed_filters \
+drupal/diff \
+drupal/views_bulk_operations \
+drupal/pathauto \
+drupal/paragraphs \
+drupal/module_filter \
+drupal/ctools \
+drupal/entity_usage \
+drupal/bootstrap
+

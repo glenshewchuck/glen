4b275a6b72852f4a5a0e2b1f0a7f7faf21c7b42f
Glen Shewchuck <glenshewchuck@gmail.com>
2020-08-20 11:40:49 -0700

    added sync, private file systems

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -5,11 +5,11 @@ $settings['trusted_host_patterns'] = [
 '^glen\.build$',
 ];
 
-$settings['file_private_path'] = 'sites/default/files/private';
+$settings['file_private_path'] = '/Users/glenshewchuck/Sites/glen/private';
 
 $settings['skip_permissions_hardening'] = TRUE;
 
-$settings['config_sync_directory'] = 'sites/sync';
+$settings['config_sync_directory'] = '/Users/glenshewchuck/Sites/glen/sync';
 
 # this sets the color for the environmental_indicator module
 $config['environment_indicator.indicator']['bg_color'] = 'green';
